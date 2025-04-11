9cd2026d4c23cf3ed34def264dac55f253e6a25f
Laurence MacGuire <leprechaun@gmail.com>
2025-04-12 01:48:57 +0700

    fix the test

--- tests/test_argument_parser.py
+++ tests/test_argument_parser.py
@@ -3,5 +3,5 @@ from mqtt_to_kafka import main
 
 def test_argument_parser_fails_with_too_few_arguments():
     with pytest.raises(SystemExit) as e:
-        main([])
+        main.main([])
     assert e.value.code == 2
